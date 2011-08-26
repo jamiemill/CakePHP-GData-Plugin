77a4de2f84588e7e06d8b1be330e078c55276d0d
Fahad Ibnay Heylaal <fahad19@gmail.com>
2011-08-26 17:00:52 +0100

    model method for saving individual contact.

--- models/google_contact.php
+++ models/google_contact.php
@@ -66,7 +66,77 @@ class GoogleContact extends GdataAppModel {
 		return $results;
 	}
 
-	
+	public function saveContact($data) {
+		$contact = new DOMDocument('1.0', 'utf-8');
+		$entry = $contact->createElementNS('http://www.w3.org/2005/Atom', 'atom:entry');
+
+		$id = $contact->createElement('id', $data['entry']['id']);
+		$entry->appendChild($id);
+
+		$category = $contact->createElement('atom:category');
+		$category->setAttribute('scheme' ,'http://schemas.google.com/g/2005#kind');
+		$category->setAttribute('term' ,'http://schemas.google.com/docs/2007#contact');
+		$entry->appendChild($category);
+
+		if (isset($data['entry']['email'])) {
+			$emails = $data['entry']['email'];
+			if (!isset($emails['0'])) {
+				$emails = array($emails);
+			}
+
+			foreach ($emails AS $email) {
+				$element = $contact->createElement('gd:email');
+
+				// address
+				$element->setAttribute('address', $email['address']);
+
+				// rel
+				if (isset($email['rel'])) {
+					$element->setAttribute('rel', $email['rel']);
+				}
+
+				// primary
+				if (!empty($email['primary'])) {
+					$element->setAttribute('primary', true);
+				}
+				$entry->appendChild($element);
+			}
+		}
+
+		$contact->appendChild($entry);
+		$body = $contact->saveXML();
+
+		$entryIdE = explode('base/', $data['entry']['id']);
+		$contactId = $entryIdE['1'];
+		$this->request = array(
+			'uri' => array(
+				'path' => 'm8/feeds/contacts/default/full/' . $contactId,
+			),
+			'header' => array(
+				'Content-Type' => 'application/atom+xml',
+				'Slug' => $data['entry']['title'],
+			),
+			'auth' => array(
+				'method' => 'OAuth',
+			),
+			'body' => $body,
+		);
+
+		$result = parent::save($data);
+
+		if($result){
+			// In Google's documentation it looks like there should be a gd:resourceId node, but it appears
+			// as simply resourceId to us. Keep an eye on this.
+			if(empty($this->response['entry']['id'])) {
+				trigger_error('No contact id from google.');
+				return false;
+			}
+			$this->setInsertID($this->response['entry']['id']);
+		}
+
+		return $result;
+	}
+
 }
 
 ?>
